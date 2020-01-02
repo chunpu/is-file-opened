8937b1fc6a030a68e7f7e1c18a8e0862079a27de
fengtong <fengtong@powerlaw.ai>
2020-01-02 20:19:08 +0800

    support not exist file

--- index.js
+++ index.js
@@ -34,7 +34,7 @@ function OpenedFilesPromise(files) {
 
 function OpenedFilePromise(file) {
   return new Promise(function(resolve, reject) {
-    Opened.file(files, function(err, ret) {
+    Opened.file(file, function(err, ret) {
       if (err) {
         // return false if crash
         return resolve(false)
